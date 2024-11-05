4c2be6b7ab5730624c18c94a362faa894491e101
Md Nahid <nahid98learn@gmail.com>
2024-11-05 09:08:13 +0600

    fs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: {
+    index: 'src/index.ts',
+    button: 'src/button.tsx',
+    input: 'src/input.tsx',
+  },
   format: ['esm'],
   dts: true,
   clean: true,
