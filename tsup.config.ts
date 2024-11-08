44c578401721a5ab3874814e2f202619f92c8e84
Md Nahid <nahid98learn@gmail.com>
2024-11-08 17:45:46 +0600

    use client added to all  chunk files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,58 @@
 import { defineConfig } from 'tsup';
+import fs from 'fs';
+import path from 'path';
+
+function readFilesRecursively(directory: string) {
+  const files: string[] = [];
+
+  function read(directory: string) {
+    const entries = fs.readdirSync(directory);
+
+    entries.forEach((entry) => {
+      const fullPath = path.join(directory, entry);
+      const stat = fs.statSync(fullPath);
+
+      if (stat.isDirectory()) {
+        read(fullPath);
+      } else {
+        files.push(fullPath);
+      }
+    });
+  }
+
+  read(directory);
+  return files;
+}
+
+async function addDirectivesToChunkFiles(distPath = 'dist'): Promise<void> {
+  try {
+    const files = readFilesRecursively(distPath);
+
+    for (const file of files) {
+      /**
+       * Skip files that are not chunk files
+       * */
+      const isIgnoreFile = !file.includes('chunk-');
+
+      if (isIgnoreFile) {
+        console.log(`⏭️ Directive 'use client'; has been skipped for ${file}`);
+        continue;
+      }
+
+      const filePath = path.join('', file);
+
+      const data = await fs.promises.readFile(filePath, 'utf8');
+
+      const updatedContent = `"use client";${data}`;
+
+      await fs.promises.writeFile(filePath, updatedContent, 'utf8');
+
+      console.log(`💚 Directive 'use client'; has been added to ${file}`);
+    }
+  } catch (err) {
+    console.error('⚠️ Something error:', err);
+  }
+}
 
 export default defineConfig({
   entry: {
@@ -52,12 +106,13 @@ export default defineConfig({
   clean: true,
   minify: true,
   sourcemap: false,
+  splitting: true,
   treeshake: true,
   bundle: true,
-  external: ['react', 'react-dom', 'node_modules'],
-  esbuildOptions(options) {
-    options.banner = {
-      js: '"use client"',
-    };
+  minifyWhitespace: true,
+  external: ['react', 'react-dom', 'tailwindcss'],
+  esbuildPlugins: [],
+  onSuccess: async () => {
+    await addDirectivesToChunkFiles();
   },
 });
